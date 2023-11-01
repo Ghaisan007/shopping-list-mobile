192394e34a763375216013f1fa4b8bd8d6559b85
Ghaisan007 <gl.aqila04@gmail.com>
2023-11-01 14:38:54 +0700

    Navbar

--- lib/menu.dart
+++ lib/menu.dart
@@ -15,7 +15,10 @@ class MyHomePage extends StatelessWidget {
       appBar: AppBar(
         title: const Text(
           'Shopping List',
+          style: TextStyle(color: Colors.white),
         ),
+        backgroundColor: Colors.indigo,
+
       ),
       body: SingleChildScrollView(
         // Widget wrapper yang dapat discroll
